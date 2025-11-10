e857f9e4a62fcdd6f03fd5e45ffb3a57f202ec46
TawsifHossain007 <tawsifhossain18.o@gmail.com>
2025-11-10 23:07:16 +0600

    Setup Done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router";
+import { Sun, Moon } from "lucide-react";
 
 const Navbar = () => {
-    return (
-        <div>
-            I am Navbar
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => setTheme(theme === "light" ? "dark" : "light");
+
+  const links = (
+    <>
+      <li><NavLink to="/">Home</NavLink></li>
+      <li><NavLink to="/allMovies">All Movies</NavLink></li>
+      <li><NavLink to="/myCollection">My Collection</NavLink></li>
+    </>
+  );
+
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+            </svg>
+          </div>
+          <ul tabIndex="-1" className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+            {links}
+          </ul>
         </div>
-    );
+        <a className="btn btn-ghost text-xl">MovieHub</a>
+      </div>
+
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{links}</ul>
+      </div>
+
+      <div className="navbar-end">
+        {/* Theme toggle */}
+        <button onClick={toggleTheme} className="btn btn-ghost">
+          {theme === "light" ? <Moon size={20} /> : <Sun size={20} />}
+        </button>
+        <button className="btn ml-0 md:ml-5">Login</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,30 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import Root from './Layout/Root';
+import Home from './Pages/Home';
+import AllMovies from './Pages/AllMovies';
+import MyCollection from './Pages/MyCollection';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: Root,
+    children: [
+      {
+        index: true,
+        path: "/",
+        Component: Home
+      },
+      {
+        path: "/allMovies",
+        Component: AllMovies
+      },
+      {
+        path: "/myCollection",
+        Component: MyCollection
+      }
+    ]
   },
 ]);
 
